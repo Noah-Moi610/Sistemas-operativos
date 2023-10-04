cf910304d1a694e4e8a1227cbb0b985591aaa627
Moi <143442529+Noah-Moi610@users.noreply.github.com>
2023-10-03 21:54:14 -0600

    Add files via upload

---
+++ JS/myscript.js
@@ -0,0 +1,20 @@
+var nav=document.querySelector('nav');
+    window.addEventListener('scroll', function(){
+       if(window.pageYOffset > 50){
+        nav.style.backgroundColor = 'rgba(180, 64, 16, 0.8)';
+        nav.classList.add('shadow');
+       }else{
+        nav.style.backgroundColor = 'transparent';
+        nav.classList.remove('shadow');
+       }
+    })
+
+
+    
+
+/*--------------------------AOS--------------------------*/
+AOS.init({
+  easing: 'ease-out-back',
+  duration: 1000
+});
+/*--------------------------/AOS--------------------------*/  
